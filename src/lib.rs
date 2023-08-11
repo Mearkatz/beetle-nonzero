83ce4dae3dba24b199be2f73e1d80c52eb03c11e
Mearkatz <drquinsey@gmail.com>
2023-08-11 07:05:40 -0400

    fixed issues with shl, shr, and trailing/zeros

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,24 @@ pub struct NonZero<T: Uint> {
     value: T,
 }
 
+impl<T: Uint> NonZero<T> {
+    /// Returns a new NonZero<T> if `value` is non-zero, else None
+    pub fn new(value: T) -> Option<Self> {
+        if value.is_zero() {
+            None
+        } else {
+            Some(Self { value })
+        }
+    }
+
+    pub fn is_even(&self) -> bool {
+        self.value.is_even()
+    }
+    pub fn is_odd(&self) -> bool {
+        self.value.is_odd()
+    }
+}
+
 macro_rules! impl_from_primitive {
     ($new_name: ty, $primitive: ty) => {
         impl From<$primitive> for $new_name {
@@ -35,87 +53,112 @@ impl<T: Uint> One for NonZero<T> {
 }
 
 pub mod ops {
-    use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Sub, SubAssign};
+    use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Shl, Shr, Sub, SubAssign};
 
-    use num::{traits::Pow, BigUint, PrimInt};
+    use num::{traits::Pow, BigUint};
 
     use crate::{traits::Uint, NonZero};
 
     // NonZero<u*> Primitive impl's
-    impl<T: Uint + PrimInt> NonZero<T> {
-        pub fn trailing_zeros(&self) -> u64 {
-            self.value.trailing_zeros().into()
-        }
 
-        pub fn leading_zeros(&self) -> u64 {
-            self.value.leading_zeros().into()
-        }
+    /// Generates an impl block for the trailing(or leading) ones(or zeros) methods
+    macro_rules! impl_ones_and_zeros {
+        ($name: ty) => {
+            impl $name {
+                /// The number of trailing zeros in the binary representation of the integer
+                pub fn trailing_zeros(&self) -> u64 {
+                    self.value.clone().trailing_zeros().into()
+                }
 
-        pub fn trailing_ones(&self) -> u64 {
-            self.value.trailing_ones().into()
-        }
+                /// The number of leading zeros in the binary representation of the integer
+                pub fn leading_zeros(&self) -> u64 {
+                    self.value.clone().leading_zeros().into()
+                }
 
-        pub fn leading_ones(&self) -> u64 {
-            self.value.leading_ones().into()
-        }
+                /// The number of trailing ones in the binary representation of the integer
+                pub fn trailing_ones(&self) -> u64 {
+                    self.value.clone().trailing_ones().into()
+                }
 
-        pub fn shift_right(&self, rhs: usize) -> Self {
-            Self {
-                value: self.value >> rhs,
+                /// The number of leading ones in the binary representation of the integer
+                pub fn leading_ones(&self) -> u64 {
+                    self.value.clone().leading_ones().into()
+                }
             }
-        }
-        pub fn shift_left(&self, rhs: usize) -> Self {
-            Self {
-                value: self.value << rhs,
-            }
-        }
+        };
     }
 
-    // NonZero<BigUint> impls
+    impl_ones_and_zeros!(NonZero<u8>);
+    impl_ones_and_zeros!(NonZero<u16>);
+    impl_ones_and_zeros!(NonZero<u32>);
+    impl_ones_and_zeros!(NonZero<u64>);
+    impl_ones_and_zeros!(NonZero<u128>);
+    impl_ones_and_zeros!(NonZero<usize>);
+
+    // For reasons I'm unaware, BigUint does not have leading_ones or leading_zeros methods, so we must leave them out
     impl NonZero<BigUint> {
+        #[doc = " The number of trailing zeros in the binary representation of the integer"]
         pub fn trailing_zeros(&self) -> u64 {
-            self.value.trailing_zeros()
+            self.value.clone().trailing_zeros().unwrap_or(0)
         }
-
-        pub fn leading_zeros(&self) -> u64 {
-            self.value.leading_zeros().into()
-        }
-
+        #[doc = " The number of trailing ones in the binary representation of the integer"]
         pub fn trailing_ones(&self) -> u64 {
-            self.value.trailing_ones().into()
+            self.value.clone().trailing_ones()
         }
+    }
 
-        pub fn leading_ones(&self) -> u64 {
-            self.value.leading_ones().into()
-        }
-
-        pub fn shift_right(&self, rhs: usize) -> Self {
-            Self {
-                value: self.value >> rhs,
+    /// Impl's Shl and Shr for NonZero<$name>
+    macro_rules! impl_primitive_shl_shr {
+        ($name: ty) => {
+            impl Shl for NonZero<$name> {
+                type Output = Self;
+                fn shl(self, rhs: Self) -> Self::Output {
+                    Self {
+                        value: self.value.clone() << rhs.value.clone(),
+                    }
+                }
             }
-        }
-        pub fn shift_left(&self, rhs: usize) -> Self {
+            impl Shr for NonZero<$name> {
+                type Output = Self;
+                fn shr(self, rhs: Self) -> Self::Output {
+                    Self {
+                        value: self.value.clone() >> rhs.value.clone(),
+                    }
+                }
+            }
+        };
+    }
+
+    impl_primitive_shl_shr!(u8);
+    impl_primitive_shl_shr!(u16);
+    impl_primitive_shl_shr!(u32);
+    impl_primitive_shl_shr!(u64);
+    impl_primitive_shl_shr!(u128);
+    impl_primitive_shl_shr!(usize);
+
+    impl<T> Shl<T> for NonZero<BigUint>
+    where
+        BigUint: Shl<T, Output = BigUint>,
+    {
+        type Output = Self;
+
+        fn shl(self, rhs: T) -> Self::Output {
             Self {
-                value: self.value << rhs,
+                value: self.value.clone() << rhs,
             }
         }
     }
 
-    impl<T: Uint> NonZero<T> {
-        /// Returns a new NonZero<T> if `value` is non-zero, else None
-        pub fn new(value: T) -> Option<Self> {
-            if value.is_zero() {
-                None
-            } else {
-                Some(Self { value })
-            }
-        }
+    impl<T> Shr<T> for NonZero<BigUint>
+    where
+        BigUint: Shr<T, Output = BigUint>,
+    {
+        type Output = Self;
 
-        pub fn is_even(&self) -> bool {
-            self.value.is_even()
-        }
-        pub fn is_odd(&self) -> bool {
-            self.value.is_odd()
+        fn shr(self, rhs: T) -> Self::Output {
+            Self {
+                value: self.value.clone() >> rhs,
+            }
         }
     }
 
@@ -181,15 +224,6 @@ pub mod ops {
         }
     }
 
-    impl<T: Uint> Pow<u32> for NonZero<T> {
-        type Output = Self;
-        fn pow(self, rhs: u32) -> Self::Output {
-            Self {
-                value: self.value.clone().pow(rhs),
-            }
-        }
-    }
-
     impl<T: Uint> AddAssign for NonZero<T> {
         fn add_assign(&mut self, rhs: Self) {
             *self = self.clone() + rhs
@@ -211,6 +245,15 @@ pub mod ops {
     impl<T: Uint> DivAssign for NonZero<T> {
         fn div_assign(&mut self, rhs: Self) {
             *self = self.clone() / rhs
+        }
+    }
+
+    impl<T: Uint> Pow<u32> for NonZero<T> {
+        type Output = Self;
+        fn pow(self, rhs: u32) -> Self::Output {
+            Self {
+                value: self.value.clone().pow(rhs),
+            }
         }
     }
 }
