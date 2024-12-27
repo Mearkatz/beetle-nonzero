1730450683e208c5bf627b3155ec5cd3ee5a3087
Mearkatz <drquinsey@gmail.com>
2024-12-27 04:47:48 -0500

    updated dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,11 @@
 //! Combines the Rust standard library's `NonZero` types into a single struct
 
+use num_traits::Zero;
 use std::{
     fmt::Display,
     num::{NonZeroU128, NonZeroU16, NonZeroU32, NonZeroU64, NonZeroU8, NonZeroUsize},
-    ops::{Not, Shr},
+    ops::Not,
 };
-
-use num::{Integer, PrimInt, Zero};
 
 /// An integer that is known to not equal zero.
 #[derive(Debug, Copy, Clone, PartialOrd, Ord, PartialEq, Eq)]
@@ -23,33 +22,6 @@ where
     }
 }
 
-impl<T> NonZero<T> {
-    /// Returns a new `NonZero` without checking that the provided value is nonzero.
-    /// # Safety
-    /// `value` must be known to be nonzero
-    pub const unsafe fn new_unchecked(value: T) -> Self {
-        Self { value }
-    }
-
-    /// A reference to the nonzero value
-    pub const fn get(&self) -> &T {
-        &self.value
-    }
-
-    /// A mutable reference to the nonzero value
-    /// # Safety
-    /// The caller must guarantee that the value is nonzero when the mutable reference is dropped
-    #[deprecated(since = "0.3.14", note = "use `swap` instead")]
-    pub const unsafe fn get_mut(&mut self) -> &mut T {
-        &mut self.value
-    }
-
-    /// Swap the nonzero value of two `NonZero`s
-    pub fn swap(&mut self, other: &mut Self) {
-        std::mem::swap(self, other);
-    }
-}
-
 impl<T> NonZero<T>
 where
     T: Zero,
@@ -59,6 +31,13 @@ where
         value.is_zero().not().then_some(Self { value })
     }
 
+    /// Returns a new `NonZero` without checking that the provided value is nonzero.
+    /// # Safety
+    /// `value` must be known to be nonzero
+    pub const unsafe fn new_unchecked(value: T) -> Self {
+        Self { value }
+    }
+
     /// Tries replacing the nonzero value with a new one.
     /// If the new value is nonzero this returns the old value,
     /// otherwise this returns None.
@@ -66,21 +45,6 @@ where
         let mut other = Self::new(new_value)?;
         self.swap(&mut other);
         Some(other.value)
-    }
-}
-
-impl<T> NonZero<T>
-where
-    T: Integer,
-{
-    /// Whether the nonzero integer is even
-    pub fn is_even(self) -> bool {
-        self.value.is_even()
-    }
-
-    /// Whether the nonzero integer is odd
-    pub fn is_odd(self) -> bool {
-        self.value.is_odd()
     }
 
     /// Sets `self.value` using the provided value.
@@ -114,21 +78,23 @@ where
     pub unsafe fn map_unchecked(self, f: impl Fn(T) -> T) -> Self {
         Self::new_unchecked(f(self.value))
     }
-}
 
-impl<T> NonZero<T>
-where
-    T: PrimInt + Shr<u32, Output = T>,
-{
-    /// Returns the number of trailing zeros in the binary representation of the nonzero integer
-    pub fn trailing_zeros(self) -> u32 {
-        self.value.trailing_zeros()
+    /// A reference to the nonzero value
+    pub const fn get(&self) -> &T {
+        &self.value
     }
 
-    /// Returns the number of trailing zeros in the binary representation of the nonzero integer
-    #[must_use]
-    pub fn without_trailing_zeros(self) -> Self {
-        unsafe { Self::new_unchecked(self.value >> self.trailing_zeros()) }
+    /// A mutable reference to the nonzero value
+    /// # Safety
+    /// The caller must guarantee that the value is nonzero when the mutable reference is dropped
+    #[deprecated(since = "0.3.14", note = "use `swap` instead")]
+    pub const unsafe fn get_mut(&mut self) -> &mut T {
+        &mut self.value
+    }
+
+    /// Swap the nonzero value of two `NonZero`s
+    pub fn swap(&mut self, other: &mut Self) {
+        std::mem::swap(self, other);
     }
 }
 
@@ -148,22 +114,3 @@ impl_from_primitive!(NonZero<u32>, NonZeroU32);
 impl_from_primitive!(NonZero<u64>, NonZeroU64);
 impl_from_primitive!(NonZero<u128>, NonZeroU128);
 impl_from_primitive!(NonZero<usize>, NonZeroUsize);
-
-#[allow(unused_imports, clippy::unwrap_used)]
-mod tests {
-    use std::ops::Range;
-
-    use num::{BigInt, FromPrimitive, One};
-
-    use crate::NonZero;
-
-    #[test]
-    fn new_works() {
-        assert!(NonZero::new(1u8).is_some());
-        assert!(NonZero::new(1u16).is_some());
-        assert!(NonZero::new(1u32).is_some());
-        assert!(NonZero::new(1u64).is_some());
-        assert!(NonZero::new(1u128).is_some());
-        assert!(NonZero::new(1usize).is_some());
-    }
-}
